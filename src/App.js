b46e221d93ad66815b973b3b7bf13d05296c3366
Josh Crowe <josh.crowe@onthebeach.co.uk>
2021-06-14 15:39:51 +0100

    Added a way to convert a click position to a Cartesian3

--- src/App.js
+++ src/App.js
@@ -1,33 +1,51 @@
-import { Cartesian3 } from "cesium";
-import { Viewer, Entity } from "resium";
+import { useRef } from "react";
+import { Cartesian2, Cartesian3, ScreenSpaceEventType } from "cesium";
+import { Viewer, Entity, ScreenSpaceEvent, ScreenSpaceEventHandler } from "resium";
 import "./App.css";
-import Plane from "./components/plane/Plane";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const position = Cartesian3.fromDegrees(-74.0707383, 40.7117244, 100);
+const dotPosition = Cartesian3.fromDegrees(-74.0707383, 40.7117244, 0);
 const pointGraphics = { pixelSize: 10 };
 
 const dummyCredit = document.createElement("div");
 
+const screenClickToCartesian3 = (current, x, y) => {
+  const scene = current?.cesiumElement?.scene;
+  if (!scene) return;
+  const ellipsoid = scene.globe.ellipsoid;
+  return scene.camera.pickEllipsoid(new Cartesian2(x, y), ellipsoid);
+}
+
 function App() {
+  const currentCesium = useRef(null);
+
+  const check = ({ position: clickPosition }) => {
+    const screenClickAsCartesian3 = screenClickToCartesian3(currentCesium.current, clickPosition.x, clickPosition.y);
+    console.log("Clicked on position!")
+    // TODO: Open popover on this position. How do we do this?
+  }
+
   return (
     <div className="App">
-      <Viewer
-        full
-        creditContainer={dummyCredit}
-        timeline={false}
-        animation={false}
-        fullscreenButton={false}
-        sceneModePicker={false}
-        baseLayerPicker={true}
-        projectionPicker={false}
-        navigationHelpButton={false}
-        homeButton={false}
-        geocoder={false}
-      >
-        <Entity position={position} point={pointGraphics} />
-        <Plane longitude={-0.124625} latitude={51.510357} elevation={100000} />
-      </Viewer>
+        <Viewer
+          full
+          ref={currentCesium}
+          creditContainer={dummyCredit}
+          timeline={false}
+          animation={false}
+          fullscreenButton={false}
+          sceneModePicker={false}
+          baseLayerPicker={true}
+          projectionPicker={false}
+          navigationHelpButton={false}
+          homeButton={false}
+          geocoder={false}
+        >
+          <ScreenSpaceEventHandler>
+            <ScreenSpaceEvent type={ScreenSpaceEventType.RIGHT_CLICK} action={check} />
+          </ScreenSpaceEventHandler>
+          <Entity onClick={check} position={dotPosition} point={pointGraphics} />
+        </Viewer>
     </div>
   );
 }
