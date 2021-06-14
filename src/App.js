4f8cfb30e2b15a4e6c45620184fea58d552f9542
tiberiushunter <s.welek@outlook.com>
2021-06-14 16:19:47 +0100

    Mapping UK polygon to globe, adding context for Viewer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import { Container, Row, Col } from "react-bootstrap";
-
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import SideBar from "./components/nav/SideBar.js";
 import Globe from "./components/globe/Globe.js";
 import "./components/nav/SideBar.css";
 import OtbImage from "./components/otbimage/OtbImage";
+import CesiumContext, { state }  from "./CesiumContext"
 
 function App() {
   return (
     <div className="App">
-      <SideBar />
-      <Globe />
-      <OtbImage />
+      <CesiumContext.Provider value={state}>
+        <SideBar />
+        <Globe />
+        <OtbImage />
+      </CesiumContext.Provider>
     </div>
   );
 }
