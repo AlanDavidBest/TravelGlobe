77eea3716aeb67cbbdf64052af058752535ff67f
Sam Welek <s.welek@outlook.co.uk>
2021-06-14 11:02:18 +0100

    Adding Plane Model

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Cartesian3 } from "cesium";
-import { Viewer, Entity } from "resium";
-import './App.css';
+import { Viewer, Entity, Model } from "resium";
+import "./App.css";
+import Plane from "./components/plane/Plane";
 
 const position = Cartesian3.fromDegrees(-74.0707383, 40.7117244, 100);
 const pointGraphics = { pixelSize: 10 };
@@ -10,20 +11,21 @@ const dummyCredit = document.createElement("div");
 function App() {
   return (
     <div className="App">
-      <Viewer 
-          full 
-          creditContainer={dummyCredit} 
-          timeline={false} 
-          animation={false} 
-          fullscreenButton={false} 
-          sceneModePicker={false}
-          baseLayerPicker={true}
-          projectionPicker={false}
-          navigationHelpButton={false}
-          homeButton={false}
-          geocoder={false}
+      <Viewer
+        full
+        creditContainer={dummyCredit}
+        timeline={false}
+        animation={false}
+        fullscreenButton={false}
+        sceneModePicker={false}
+        baseLayerPicker={true}
+        projectionPicker={false}
+        navigationHelpButton={false}
+        homeButton={false}
+        geocoder={false}
       >
         <Entity position={position} point={pointGraphics} />
+        <Plane longitude={-0.124625} latitude={51.510357} elevation={100000} />
       </Viewer>
     </div>
   );
