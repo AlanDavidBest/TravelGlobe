a7cfd4c92878bdf22f22dabfdd9cbcac6bbd1f41
Karam Kabbara <karam.kabbara@onthebeach.co.uk>
2021-06-15 15:07:47 +0100

    styling for destination slides

--- src/components/info-carousel/InfoCarousel.js
+++ src/components/info-carousel/InfoCarousel.js
@@ -2,24 +2,112 @@ import React from "react";
 import { Carousel } from "react-bootstrap";
 import "./InfoCarousel.css";
 
-export const InfoCarousel = ({ slides }) => (
-    <Carousel>
-        {
-            slides.map(slide => (
-                <Carousel.Item>
-                    <img
-                        className="d-block"
-                        src={slide.img}
-                        alt={slide.title}
-                    />
-                    <Carousel.Caption>
-                        <h3>{slide.title}</h3>
-                        <p>{slide.description}</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            ))
-        }
+export const InfoCarousel = ({ title, image, description, rating, weather, currency }) => (
+    <Carousel interval={null} indicators>
+        <Carousel.Item>
+            <Carousel.Caption>
+                <h1>{title}</h1>
+            </Carousel.Caption>
+
+            <img
+                className="d-block"
+                src={image}
+                alt={title}
+            />
+        </Carousel.Item>
+
+        <Carousel.Item>
+            <Carousel.Caption>
+                <h1>{title}</h1>
+                <div className="rating-container">
+                        {
+                            [...Array(rating)].map((s, i) => (
+                                <span key={i}>&#9733;</span>
+                            ))
+                        }
+                </div>
+
+                <h4>{description}</h4>
+            </Carousel.Caption>
+
+            <img
+                className="d-block"
+                src={image}
+                alt={title}
+            />
+        </Carousel.Item>
+
+        <Carousel.Item>
+            <WeatherWidget title={title} weather={weather} />
+
+            <img
+                className="d-block"
+                src={image}
+                alt={title}
+            />
+        </Carousel.Item>
+
+        <Carousel.Item>
+            <CurrencyWidget title={title} currency={currency} />
+
+            <img
+                className="d-block"
+                src={image}
+                alt={title}
+            />
+        </Carousel.Item>
     </Carousel>
 );
+
+
+const WeatherWidget = ({ title, weather }) => {
+    const getWeatherIcon = (temperature) => {
+        if (temperature < 8) return (<i class="fas fa-snowflake"></i>);
+        if (temperature > 15) return (<i class="fas fa-sun"></i>);
+        return (<i class="fas fa-cloud-sun"></i>);
+    }
+
+    return (
+        <Carousel.Caption>
+            <h1>{title}</h1>
+            <div class="weather-container">
+                {
+                    weather.map((w) => (
+                        <div className="weather-item">
+                            <h3>{w.name}</h3>
+                            {
+                                getWeatherIcon(w.celcius)
+                            }
+                            <h4>{`${w.celcius}'C`}</h4>
+                        </div>
+                    ))
+                }
+            </div>
+        </Carousel.Caption>
+    )
+}
+
+const CurrencyWidget = ({ title, currency }) => {
+    return (
+        <Carousel.Caption>
+            <h1>{title}</h1>
+            <div class="currency-container">
+                {
+                    currency.map((c) => (
+                        <div className={c.atHome ? `currency-item currency-item-athome` : `currency-item`}>
+                            { c.prepend &&
+                                <h1>{`${c.icon}${c.amount}`}</h1>
+                            }
+
+                            { !c.prepend &&
+                                <h1>{`${c.amount}${c.icon}`}</h1>
+                            }
+                        </div>
+                    ))
+                }
+            </div>
+        </Carousel.Caption>
+    )
+}
 
 export default InfoCarousel;
--- src/components/info/Info.js
+++ src/components/info/Info.js
@@ -3,28 +3,31 @@ import { InfoCarousel } from "../info-carousel/InfoCarousel";
 import "./Info.css";
 
 export const Info = ({ x, y }) => {
-  const slides = [
-    {
-      title: "Sunny Manchester",
-      description: "This is an awesome slide!",
-      img: "https://www.thetreecenter.com/c/uploads/palm-trees-beach-scaled-2048x1146.jpg"
-    },
-    {
-      title: "Sunny Manchester",
-      description: "This is an awesome slide!",
-      img: "https://www.thetreecenter.com/c/uploads/palm-trees-beach-scaled-2048x1146.jpg"
-    },
-    {
-      title: "Sunny Manchester",
-      description: "This is an awesome slide!",
-      img: "https://www.thetreecenter.com/c/uploads/palm-trees-beach-scaled-2048x1146.jpg"
-    }
-  ]
-  
+  const destination =
+  {
+    title: "Sunny Manchester",
+    image: "https://www.thetreecenter.com/c/uploads/palm-trees-beach-scaled-2048x1146.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget diam ut purus venenatis molestie. Sed mollis dui et arcu blandit tincidunt.",
+    rating: 2,
+    weather: [
+      { name: "Spring", celcius: 11 },
+      { name: "Summer", celcius: 200 },
+      { name: "Autumn", celcius: 11 },
+      { name: "Winter", celcius: 11 }
+    ],
+    currency: [
+      { icon: "£", amount: 1, atHome: true, prepend: true },
+      { icon: "$", amount: 1.5, atHome: false, prepend: true },
+      { icon: "€", amount: 1.5, atHome: false, prepend: true },
+      { icon: "¥", amount: 1.5, atHome: false, prepend: false },
+      { icon: "Fr", amount: 1.5, atHome: false, prepend: false },
+    ]
+  };
+
   return (
     <div className="infoContainer" style={{ top: `${y}px`, left: `${x}px` }}>
       <div className="infoCarouselContainer">
-        <InfoCarousel slides={slides} />
+        <InfoCarousel {...destination} />
       </div>
     </div>
   );
