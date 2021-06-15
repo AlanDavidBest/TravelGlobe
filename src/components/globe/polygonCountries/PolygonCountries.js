33ab7efe6f50b3b2769eab011fe3097ba274f440
Alan Best <AlanDavidBest@gmail.com>
2021-06-15 17:14:53 +0100

    make_polygons_transparent

--- src/components/globe/polygonCountries/PolygonCountries.js
+++ src/components/globe/polygonCountries/PolygonCountries.js
@@ -10,7 +10,8 @@ class PolygonCountries extends React.Component {
   }
 
   randomColor() {
-    return new Color(Math.random(), Math.random(), Math.random(), 0.5);
+    //return new Color(Math.random(), Math.random(), Math.random(), 0.5);
+    return new Color(1.0, 1.0, 0.0, 0);
   }
   render() {
     return (
