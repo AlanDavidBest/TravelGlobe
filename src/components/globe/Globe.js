2152cb893b5b50caa4963c4eca7b8b8290dd1380
tiberiushunter <s.welek@outlook.com>
2021-06-15 13:15:14 +0100

    Adding in Capital City search with Cards

--- src/components/globe/Globe.js
+++ src/components/globe/Globe.js
@@ -1,32 +1,30 @@
 import React, { createRef } from "react";
-import { Cartesian3, Color, buildModuleUrl} from "cesium";
+import { Cartesian3, Color, buildModuleUrl } from "cesium";
 import { Viewer, Entity, PolygonGraphics } from "resium";
 import Plane from "../plane/Plane";
 import CesiumContext from "../../CesiumContext";
 import countries from "../../data/countries.geo.json";
 
-
 const position = Cartesian3.fromDegrees(-74.0707383, 40.7117244, 100);
 const pointGraphics = { pixelSize: 10 };
 const dummyCredit = document.createElement("div");
 
-
 function useClickedItem(e) {
-    console.log("E " + JSON.stringify(e));
+  console.log("E " + JSON.stringify(e));
 }
 
 function randomColor() {
-    return new Color(Math.random(), Math.random(), Math.random(), 0.0);
+  return new Color(Math.random(), Math.random(), Math.random(), 0.0);
 }
 
-export default class Global extends React.Component {
-    constructor(props) {
-        super(props);
-        this.ref = createRef();
-        this.selected = []
-        this.selected["GBR"] = true;
-        this.randomColor = randomColor
-    }
+class Global extends React.Component {
+  constructor(props) {
+    super(props);
+    this.ref = createRef();
+    this.selected = [];
+    this.selected["GBR"] = true;
+    this.randomColor = randomColor;
+  }
 
   componentDidMount() {
     if (this.ref.current && this.ref.current.cesiumElement) {
@@ -43,19 +41,18 @@ export default class Global extends React.Component {
           cssLink.type = "text/css";
           frame.contentDocument.head.appendChild(cssLink);
 
-          var x = document.createElement('div')
-          x.classList = "cesium-infoBox-description"
-          x.innerText = "Other fancy content"
-          
-          frame.contentDocument.body.appendChild(x);
+          var x = document.createElement("div");
+          x.classList = "cesium-infoBox-description";
+          x.innerText = "Other fancy content";
 
+          frame.contentDocument.body.appendChild(x);
         },
         false
       );
     }
   }
 
-// Estonia  Tallinn 59.22N  24.48E
+  // Estonia  Tallinn 59.22N  24.48E
 
   render() {
     return (
@@ -76,46 +73,58 @@ export default class Global extends React.Component {
         selectionIndicator={false}
         onClick={useClickedItem}
       >
-        
-            <Entity> {
-                countries.features.filter(country => country.geometry.type === 'Polygon').map((country, i) => {
-                    var countryCoordinates = [].concat.apply([], country.geometry.coordinates[0]);
-                    return (
-                        <Entity key={"E" + country.id + i.toString()} name={country.id} description={country.properties.name}>
-                            <PolygonGraphics
-                                key={country.id + i.toString()}
-                                hierarchy={Cartesian3.fromDegreesArray(countryCoordinates)} 
-                                fill={true} 
-                                material={this.randomColor()} 
-                                />;
-                        </Entity>
-                    )
-                })
-            }
-            </Entity>
-            <Entity> {
-                countries.features.filter(country => country.geometry.type === 'MultiPolygon').map((country, i) => {
-                    return (
-                            country.geometry.coordinates.map((polygon, j) => {
-                                var  polyCoords = [].concat.apply([], polygon[0])
-                                return (
-                                    <Entity key={"E" + country.id + i.toString() + "-" + j.toString()} name={country.id} description={country.properties.name}>
-                                        <PolygonGraphics 
-                                            key={country.id + i.toString() + "-" + j.toString()}
-                                            hierarchy={Cartesian3.fromDegreesArray(polyCoords)} 
-                                            fill={true} 
-                                            material={ this.selected[country.id] ? Color.RED : this.randomColor() } 
-                                            />;
-                                    </Entity>
-                                )
-                            }
-                            )
-                    )
-                        }
-                )
-            }
-            </Entity>
-       
+        <Entity>
+          {countries.features
+            .filter((country) => country.geometry.type === "Polygon")
+            .map((country, i) => {
+              var countryCoordinates = [].concat.apply(
+                [],
+                country.geometry.coordinates[0]
+              );
+              return (
+                <Entity
+                  key={"E" + country.id + i.toString()}
+                  name={country.id}
+                  description={country.properties.name}
+                >
+                  <PolygonGraphics
+                    key={country.id + i.toString()}
+                    hierarchy={Cartesian3.fromDegreesArray(countryCoordinates)}
+                    fill={true}
+                    material={this.randomColor()}
+                  />
+                </Entity>
+              );
+            })}
+        </Entity>
+        <Entity>
+          {countries.features
+            .filter((country) => country.geometry.type === "MultiPolygon")
+            .map((country, i) => {
+              return country.geometry.coordinates.map((polygon, j) => {
+                var polyCoords = [].concat.apply([], polygon[0]);
+                return (
+                  <Entity
+                    key={"E" + country.id + i.toString() + "-" + j.toString()}
+                    name={country.id}
+                    description={country.properties.name}
+                  >
+                    <PolygonGraphics
+                      key={country.id + i.toString() + "-" + j.toString()}
+                      hierarchy={Cartesian3.fromDegreesArray(polyCoords)}
+                      fill={true}
+                      material={
+                        this.selected[country.id]
+                          ? Color.RED
+                          : this.randomColor()
+                      }
+                    />
+                  </Entity>
+                );
+              });
+            })}
+        </Entity>
+
         <Entity position={position} point={pointGraphics} />
 
         <Plane longitude={-0.124625} latitude={51.510357} elevation={100000} />
@@ -124,3 +133,4 @@ export default class Global extends React.Component {
   }
 }
 Global.contextType = CesiumContext;
+export default Global;
--- src/components/nav/SideBar.js
+++ src/components/nav/SideBar.js
@@ -1,23 +1,53 @@
 import React from "react";
-import { Nav, Form, FormControl, Button } from "react-bootstrap";
+import { Nav, Card } from "react-bootstrap";
+import Search from "./search/Search";
 import "./SideBar.css";
 
-function SideBar() {
-  return (
-    <Nav
-      className="col-md-12 d-none d-md-block bg-dark sidebar"
-      activeKey="/home"
-      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-    >
-      <div className="sidebar-sticky"></div>
-      <Nav.Item>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
-      </Nav.Item>
-    </Nav>
-  );
+class SideBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      matchedCities: [],
+    };
+
+    this.handleMatchedCities = this.handleMatchedCities.bind(this);
+  }
+
+  handleMatchedCities(matches) {
+    console.log(matches);
+    this.setState({ matchedCities: matches });
+  }
+
+  render() {
+    return (
+      <Nav
+        className="col-md-12 d-none d-md-block bg-dark sidebar"
+        activeKey="/home"
+        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+      >
+        <div className="sidebar-sticky"></div>
+        <Nav.Item>
+          <Search onSearch={this.handleMatchedCities} />
+        </Nav.Item>
+
+        {this.state.matchedCities.map((item) => {
+          return (
+            <Nav.Item>
+              <Card>
+                <Card.Img variant="top" src="https://picsum.photos/200/100" />
+                <Card.Body>
+                  <Card.Title>{item.city}</Card.Title>
+                  <Card.Text>
+                    {item.country}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Nav.Item>
+          );
+        })}
+      </Nav>
+    );
+  }
 }
 
 export default SideBar;
